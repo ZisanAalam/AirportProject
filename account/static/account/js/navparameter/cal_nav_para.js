8d810fd8913f5ac2dae159227b44b94880293bc0
Aayush Acharya <ayushacharya99@gmail.com>
2021-05-15 15:19:00 +0545

    Js for charts

--- account/static/account/js/navparameter/cal_nav_para.js
+++ account/static/account/js/navparameter/cal_nav_para.js
@@ -59,14 +59,17 @@ Array.from(allinput).forEach(item=>item.onchange=()=>{
         method : "GET",
         data:{'makes':makes,'models':models,'startdate':startDate,'enddate':endDate},
         success: function(response){
-            console.log(response)
             if(response.data==="invalid"){
                 document.getElementById('navtable-init-header').style.display="none"
                 document.getElementById('navtable-init').style.display="none"
+                document.getElementById('radarChart').style.display="none"
                 document.getElementById('navtable-header').style.display="none"
                 document.getElementById('navtable').style.display="none"
+                document.getElementById('lineChart').style.display="none"
             }
             else{
+                document.getElementById('radarChart').remove()
+                document.getElementById('lineChart').remove()
                 all_rows_dom=document.querySelectorAll('tr')
                 all_headers_dom=document.querySelectorAll('thead')
                 for (var i=0; i<all_rows_dom.length;i++){
@@ -74,56 +77,17 @@ Array.from(allinput).forEach(item=>item.onchange=()=>{
                         all_rows_dom[i].remove()
                     }
                 }
-                init_entries=response.navparams
-                init_entries.forEach((item,index)=>{
-                    const each_row=document.createElement('tr')
-                    each_row.setAttribute('id',`navinit${index}`)
-                    const equipment=document.createElement('td')
-                    equipment.innerHTML=item.equipment
-                    each_row.appendChild(equipment)
-                    const faultlocation=document.createElement('td')
-                    faultlocation.innerHTML=item.location
-                    each_row.appendChild(faultlocation)
-                    const availability=document.createElement('td')
-                    availability.innerHTML=item.availability.toFixed(5)
-                    each_row.appendChild(availability)
-                    const reliability=document.createElement('td')
-                    reliability.innerHTML=item.reliability.toFixed(5)
-                    each_row.appendChild(reliability)
-                    const integrity=document.createElement('td')
-                    integrity.innerHTML=item.integrity.toFixed(5)
-                    each_row.appendChild(integrity)
-                    const continuity=document.createElement('td')
-                    continuity.innerHTML=item.continuity.toFixed(5)
-                    each_row.appendChild(continuity)
-                    document.getElementById('navtable-init-body').appendChild(each_row)
-
-                })
-                all_entries=response.data
-                all_entries.forEach((item,index)=>{
-                    const each_row=document.createElement('tr')
-                    each_row.setAttribute('id',`nav${index}`)
-                    const equipment=document.createElement('td')
-                    equipment.innerHTML=item.equipment
-                    each_row.appendChild(equipment)
-                    const faultlocation=document.createElement('td')
-                    faultlocation.innerHTML=item.location
-                    each_row.appendChild(faultlocation)
-                    const faultlocationpart=document.createElement('td')
-                    faultlocationpart.innerHTML=item.locationpart
-                    each_row.appendChild(faultlocationpart)
-                    const nooffailures=document.createElement('td')
-                    nooffailures.innerHTML=item.num_failures
-                    each_row.appendChild(nooffailures)
-                    const failureRate=document.createElement('td')
-                    failureRate.innerHTML=(item.failure_rate*1000000.0).toFixed(4)
-                    each_row.appendChild(failureRate)
-                    document.getElementById('navtable-body').appendChild(each_row)
-                })
+                performance_table(response.navparams)
+                radar_chart(response.navparams)
+                reliability_table(response.data)
+                line_chart(response.data)
+                
                 document.getElementById('navtable-init-header').style.display="block"
                 document.getElementById('navtable-init').style.display="table"
+                document.getElementById('radarChart').style.display="inline-block"
                 document.getElementById('navtable-header').style.display="block"
                 document.getElementById('navtable').style.display="table"
+                document.getElementById('lineChart').style.display="inline-block"
             }
         },
         error: function(error){
@@ -133,3 +97,141 @@ Array.from(allinput).forEach(item=>item.onchange=()=>{
         
 });
 
+const performance_table=(init_entries)=>{
+    init_entries.forEach((item,index)=>{
+        const each_row=document.createElement('tr')
+        each_row.setAttribute('id',`navinit${index}`)
+        const availability=document.createElement('td')
+        availability.innerHTML=item.availability.toFixed(5)
+        each_row.appendChild(availability)
+        const reliability=document.createElement('td')
+        reliability.innerHTML=item.reliability.toFixed(5)
+        each_row.appendChild(reliability)
+        const integrity=document.createElement('td')
+        integrity.innerHTML=item.integrity.toFixed(5)
+        each_row.appendChild(integrity)
+        const continuity=document.createElement('td')
+        continuity.innerHTML=item.continuity.toFixed(5)
+        each_row.appendChild(continuity)
+        document.getElementById('navtable-init-body').appendChild(each_row)
+
+    })
+
+}
+
+const reliability_table=(all_entries)=>{
+    all_entries.forEach((item,index)=>{
+        const each_row=document.createElement('tr')
+        each_row.setAttribute('id',`nav${index}`)
+        const equipment=document.createElement('td')
+        equipment.innerHTML=item.equipment
+        each_row.appendChild(equipment)
+        const faultlocation=document.createElement('td')
+        faultlocation.innerHTML=item.location
+        each_row.appendChild(faultlocation)
+        const faultlocationpart=document.createElement('td')
+        faultlocationpart.innerHTML=item.locationpart
+        each_row.appendChild(faultlocationpart)
+        const nooffailures=document.createElement('td')
+        nooffailures.innerHTML=item.num_failures
+        each_row.appendChild(nooffailures)
+        const failureRate=document.createElement('td')
+        failureRate.innerHTML=(item.failure_rate*1000000.0).toFixed(4)
+        each_row.appendChild(failureRate)
+        document.getElementById('navtable-body').appendChild(each_row)
+    })
+
+}
+
+/* ------------------------------------ Graphical analysis ------------------------------ */
+
+const radar_chart=(response_data)=>{
+    
+    
+    data={
+        labels: ['Availability','Reliability','Integrity','Continuity'],
+        datasets: [{
+            data: !response_data.length ? [] : [response_data[0].availability,response_data[0].reliability,response_data[0].integrity, response_data[0].continuity] ,
+            fill: true,
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgb(54, 162, 235)',
+            pointBackgroundColor: 'rgb(54, 162, 235)',
+            pointRadius: 6,
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgb(54, 162, 235)'
+        }]
+    }
+    
+    const config = {
+        type: 'radar',
+        data: data,
+        options: {
+            plugins:{
+                legend: {
+                    display: false
+                },
+            },
+            scales:{
+                r:{
+                    pointLabels:{
+                        padding: 20,
+                        font:{
+                            weight: 'bold',
+                            size: 15
+                        }
+                    }
+                }
+            },
+            elements: {
+                line: {
+                    borderWidth: 3
+                }
+            },
+      }
+    }
+    var chartDiv=document.getElementById('radar-chart-div');
+    var ctx=document.createElement('canvas')
+    ctx.setAttribute('id','radarChart')
+    chartDiv.appendChild(ctx)
+    var radarChart=new Chart(ctx,config)
+
+    
+}
+
+const line_chart=(response_data)=>{
+    
+    var labels=response_data.map(item=>{
+        return `${item.equipment}, ${item.locationpart}`
+    })
+    var dataset=response_data.map(item=>{
+        return item.failure_rate*1000000.0.toFixed(4)
+    })
+    const data = {
+        labels: labels,
+        datasets: [{
+          data: dataset,
+          fill: false,
+          backgroundColor: response_data.map(()=>'rgb(75, 192, 192)'),
+          tension: 0.1
+        }]
+      };
+    const config = {
+        type: 'bar',
+        data: data,
+        options:{
+            plugins:{
+                legend: {
+                    display: false
+                },
+            },
+        }
+      };
+    var chartDiv=document.getElementById('line-chart-div');
+    var ctx=document.createElement('canvas')
+    ctx.setAttribute('id','lineChart')
+    chartDiv.appendChild(ctx)
+    var lineChart=new Chart(ctx,config)
+
+    
+}
