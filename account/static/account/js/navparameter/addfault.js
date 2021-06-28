6b27db5b32905f2ecae15fe19ebedcc878dec587
Aayush Acharya <ayushacharya99@gmail.com>
2021-06-28 22:43:26 +0545

    Added form validation

--- account/static/account/js/navparameter/addfault.js
+++ account/static/account/js/navparameter/addfault.js
@@ -2,34 +2,103 @@ $('.ui.dropdown').dropdown({
     forceSelection : false
 });
 
-/* ----------------------------- Form Validation ------------------------------- 
+/* ----------------------------- Form Validation ------------------------------- */
+function clearError(){
+    document.getElementById('runway-error').innerHTML =""
+    document.getElementById('equipment-error').innerHTML =""
+    document.getElementById('make-error').innerHTML =""
+    document.getElementById('model-error').innerHTML =""
+    document.getElementById('location-error').innerHTML =""
+    document.getElementById('module-error').innerHTML =""
+    document.getElementById('sdate-error').innerHTML =""
+    document.getElementById('edate-error').innerHTML =""
+    document.getElementById('stime-error').innerHTML =""
+    document.getElementById('etime-error').innerHTML =""
+}
 
-const form = document.getElementById('form')
-form.addEventListener('submit',(e) =>{
-    e.preventDefault()
-    checkInputs()
-});
+
+const runway_element=document.getElementById('runway')
+const equipment_element=document.getElementById('equipment')
+const make_element=document.getElementById('make')
+const model_element=document.getElementById('model')
+const location_element=document.getElementById('location')
+const module_element=document.getElementById('location-part')
+const sdate_element = document.getElementById('sdate')
+const edate_element = document.getElementById('edate')
+const stime_element = document.getElementById('stime')
+const etime_element = document.getElementById('etime')
+runway_element.onchange=clearError
+equipment_element.onchange=clearError
+make_element.onchange=clearError
+model_element.onchange=clearError
+location_element.onchange=clearError
+sdate_element.onchange=clearError
+edate_element.onchange=clearError
+stime_element.onchange=clearError
+etime_element.onchange=clearError
 
 
-function checkInputs(){
-    const sdate = document.getElementById('sdate').value()
-    const edate = document.getElementById('edate').value()
-    const stime = document.getElementById('stime').value()
-    const etime = document.getElementById('etime').value()
-    if(sdate==''){
-        document.getElementById('sdate-error').innerHTML ="Please enter start date"
+function checkInputs(event){
+    let dateregex=/^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/
+    let timeregex=/^(([0-1]\d{1})|(2[0-3])):[0-5]\d{1}:[0-5]\d{1}$/
+    const sdate = sdate_element.value
+    const edate = edate_element.value
+    const stime = stime_element.value
+    const etime = etime_element.value
+    const runway=runway_element.value
+    const equipment=equipment_element.value
+    const make=make_element.value
+    const model=model_element.value
+    const location=location_element.value
+    const module=module_element.value
+    var flag=false
+    if(runway==""){
+        document.getElementById('runway-error').innerHTML ="Please select a runway."
+        flag=true
     }
-    if(edate==''){
-        document.getElementById('sdate-error').innerHTML ="Please enter end date"
+    if(equipment==""){
+        document.getElementById('equipment-error').innerHTML ="Please select an equipment."
+        flag=true
+    }
+    if(make==""){
+        document.getElementById('make-error').innerHTML ="Please select a maker."
+        flag=true
+    }
+    if(model==""){
+        document.getElementById('model-error').innerHTML ="Please select a model."
+        flag=true
+    }
+    if(location==""){
+        document.getElementById('location-error').innerHTML ="Please select a fault location."
+        flag=true
+    }
+    if(module==""){
+        document.getElementById('module-error').innerHTML ="Please select a fault module."
+        flag=true
+    }
+    if(!dateregex.test(sdate)){
+        document.getElementById('sdate-error').innerHTML ="Please enter start date"
+        flag=true
+    }
+    if(!dateregex.test(edate)){
+        document.getElementById('edate-error').innerHTML ="Please enter end date"
+        flag=true
     } 
-    if(stime==''){
-        document.getElementById('sdate-error').innerHTML ="Please enter start time"
+    if(!timeregex.test(stime)){
+        document.getElementById('stime-error').innerHTML ="Please enter start time"
+        flag=true
     } 
-    if(etime==''){
-        document.getElementById('sdate-error').innerHTML ="Please enter end time"
+    if(!timeregex.test(etime)){
+        document.getElementById('etime-error').innerHTML ="Please enter end time"
+        flag=true
+    }
+    if(flag){
+        event.preventDefault()
+        flag=false
     }  
 }
-*/
+const form = document.getElementById('form')
+form.onsubmit=checkInputs
 
 /* ----------------------- For Equipment dropdown ----------------------------------*/
 const inputRunway = document.getElementById('runways')
